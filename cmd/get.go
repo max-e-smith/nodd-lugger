5a121a335c9c6c1c324e6c951ab2effc4aef2e6b
max-e-smith <maxwellfth@gmail.com>
2025-12-20 22:37:27 -0700

    get aws s3 sdk and init s3 client

--- cmd/get.go
+++ cmd/get.go
@@ -1,8 +1,13 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 var bathy bool
@@ -14,10 +19,10 @@ var getCmd = &cobra.Command{
 	Short: "Download NOAA survey data to local path",
 	Long: `Use 'clug get <survey(s)> <local path> <options>' to download marine geophysics data to your machine. 
 
-		Data is downloaded from the NOAA Open Data Dissemination cloud bucket by default. Use
-		the config command to view and change buckets used for each data type. You must specify a 
-		data type(s) to download by specifying one or more of the data options for this command. 
-		View the help for this command for more info on those options.`,
+		Data is downloaded from the NOAA Open Data Dissemination cloud buckets by default. You must 
+		specify a data type(s) for this command. View the help for more info on those options. Specify
+		the survey(s) you want to download and a local path to download data to. The path must exist and 
+		have the necessary permissions.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		var length = len(args)
@@ -30,9 +35,10 @@ var getCmd = &cobra.Command{
 		var path = args[length-1]
 		var surveys = args[:length-1] // high is non-inclusive
 
-		if !bathy || !wcd || !trackline {
+		if !bathy && !wcd && !trackline {
 			fmt.Println("Please specify data type(s) for download.")
 			fmt.Println(cmd.UsageString())
+			return
 		}
 
 		download(surveys, path)
@@ -52,47 +58,62 @@ func init() {
 }
 
 func download(surveys []string, path string) {
+
 	if bathy {
-		fmt.Println("resolving bathymetry data for provided surveys")
-		// TODO resolve surveys
+		fmt.Println("resolving bathymetry data for provided surveys: {}", surveys)
+		var surveyRoots []string = resolveBathySurveys(surveys)
+
 		fmt.Println("checking available disk space")
-		// TODO verify survey size against available disk space and report out
+		diskSpaceCheck(surveyRoots)
+
 		fmt.Println("downloading surveys ", surveys, " to ", path, "...")
 		// TODO recursively download surveys
+
 		fmt.Println("bathymetry data downloaded.")
 	}
 
 	if wcd {
-		fmt.Println("resolving water column data for provided surveys")
-		// TODO resolve surveys
-		fmt.Println("checking available disk space")
-		// TODO verify survey size against available disk space and report out
-		fmt.Println("downloading surveys ", surveys, " to ", path, "...")
-		// TODO recursively download surveys
-		fmt.Println("bathymetry data downloaded.")
+		// TODO
 	}
 
 	if trackline {
-		fmt.Println("resolving bathymetry data for provided surveys")
-		// TODO resolve surveys
-		fmt.Println("checking available disk space")
-		// TODO verify survey size against available disk space and report out
-		fmt.Println("downloading surveys ", surveys, " to ", path, "...")
-		// TODO recursively download surveys
-		fmt.Println("bathymetry data downloaded.")
+		// TODO
 	}
 
 	return
 }
 
-func resolveBathySurveys() {
+func diskSpaceCheck(rootPaths []string) {
 	// TODO
 }
 
-func resolveWaterColumnSurveys() {
-	// TODO
-}
+func resolveBathySurveys(surveys []string) []string {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithCredentialsProvider(aws.AnonymousCredentials{}),
+	)
+	if err != nil {
+		log.Fatal(err)
+		return []string{}
+	}
 
-func resolveTracklineSurveys() {
-	// TODO
+	client := s3.NewFromConfig(cfg)
+
+	bucket := "noaa.dcdb-bathymetry-pds.s3.amazonaws.com"
+
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+	})
+	if err != nil {
+		log.Fatal(err)
+		return []string{}
+	}
+
+	log.Println("first page results")
+	for _, object := range output.Contents {
+		log.Printf("key=%s size=%d", aws.ToString(object.Key), *object.Size)
+	}
+
+	var paths []string
+
+	return paths
 }
